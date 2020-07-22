9abffa41bcb891d6b0ecc11b78b3de83f3deb23a
Michelle Stermitz <stermitzm@gmail.com>
2020-07-22 11:57:12 -0700

    Got my chart working

--- results/results-chart.js
+++ results/results-chart.js
@@ -1,31 +1,35 @@
+import { mungeNames, mungeCaptured, mungeEncountered, getCart, clearCart } from './results-utils.js';
+
 const ctx = document.getElementById('myChart').getContext('2d');
 
+const pokemonData = getCart();
+const mungedNames = mungeNames(pokemonData);
+const mungedCaptured = mungeCaptured(pokemonData);
+const mungedEncountered = mungeEncountered(pokemonData);
 
 const myChart = new Chart(ctx, { // eslint-disable-line
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: mungedNames,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            label: 'Pokemon Captured',
+            data: mungedCaptured,
+            backgroundColor:
+                'pink',
+            borderColor:
+                'pink',
             borderWidth: 1
-        }]
+        },
+        {
+            label: 'Pokemon Encountered',
+            data: mungedEncountered,
+            backgroundColor: 
+                'yellow',
+            borderColor: 
+                'yellow',
+            borderWidth: 1
+        },
+        ]
     },
     options: {
         scales: {
