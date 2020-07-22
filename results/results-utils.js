6e4b037ed69bc7513d4ec3715edbff25656d0bc1
Michelle Stermitz <stermitzm@gmail.com>
2020-07-22 10:31:04 -0700

    Added munge functions

--- results/results-utils.js
+++ results/results-utils.js
@@ -1,25 +1,25 @@
-// export function mungeCaptured(pokemonArray) {
-//     const captures = [];
+export function mungeCaptured(pokemonArray) {
+    const captures = [];
     
-//     for (let i = 0; i < pokemonArray.length; i++) {
-//         const pokemon = pokemonArray[i];
+    for (let i = 0; i < pokemonArray.length; i++) {
+        const pokemon = pokemonArray[i];
         
-//         captures.push(pokemon.captured);
+        captures.push(pokemon.captured);
 
-//     }
+    }
     
-//     return captures;
+    return captures;
 
-// }
+}
 
-// export function mungeNames(pokemonArray) {
-//     const names = [];
+export function mungeNames(pokemonArray) {
+    const names = [];
 
-//     for (let i = 0; i < pokemonArray.length; i++) {
-//         const pokemon = pokemonArray[i];
+    for (let i = 0; i < pokemonArray.length; i++) {
+        const pokemon = pokemonArray[i];
 
-//         names.push(pokemon.pokemon);
-//     }
+        names.push(pokemon.pokemon);
+    }
     
-//     return names;
-// }
+    return names;
+}
